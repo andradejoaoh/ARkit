cd4976b8296042e6302a9db8569ce6793f6210a7
liakassardjian <liakassardjian@gmail.com>
2020-02-20 17:44:34 -0300

    Resolução de conflitos

--- QuimicaAR/EnumElementos.swift
+++ QuimicaAR/EnumElementos.swift
@@ -9,15 +9,8 @@
 import Foundation
 enum Elemento: String {
     case hidrogenio, hidrogenio1, hidrogenio2, hidrogenio3
-<<<<<<< HEAD
     case oxigenio, oxigenio1, oxigenio2, oxigenio3
     case carbono, carbono1, carbono2, carbono3
     case nitrogenio, nitrogenio1, nitrogenio2, nitrogenio3
     case fluor, fluor1, fluor2, fluor3
-=======
-    case oxigenio
-    case carbono, carbono1, carbono2, carbono3
-    case nitrogenio
-    case fluor
->>>>>>> feature/matematica
 }
